fa7d56fdd369cf1070e47f838b1535554bc0f5c0
Ryan <antonyryanr@gmail.com>
2014-10-17 10:34:44 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HorowitzRealEstate::Application.config.secret_key_base = 'fa5c1df34a51f1e89b79068bb5b0c2e5b9ee7e6070cd39ffe0b5ffd849475f9b60ffd4fc882f1605a48ef55db9e293eab25292cb7a5b081ba08e329222db757a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HorowitzRealEstate::Application.config.session_store :cookie_store, key: '_horowitz_real_estate_session'
